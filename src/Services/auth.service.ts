1c40287a4422b3fc36f67c46ee3020171c35fafa
thpacheco <thiagopacheco30@gmail.com>
2022-10-08 20:28:37 -0300

    adicionando headers

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -1,12 +1,21 @@
 import axios from "axios";
 const API_URL = "http://adoleta-api-dev.us-east-1.elasticbeanstalk.com/v1/api/";
 // const API_URL = "https://localhost:7043/v1/api/";
+
+let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+    }
+  };
+
 class AuthService {
     login(email: string, password: string) {
         return axios
             .post(API_URL + "auth", {
                 email,
-                password
+                password,
+                axiosConfig
             })
             .then(response => {
                 if (response.data.data.token) {
