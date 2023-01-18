707bcb26c04a433a053ac26e804cb458954131b3
thpacheco <thiagopacheco30@gmail.com>
2023-01-17 22:22:54 -0300

    Limpando localstorage

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
+import AuthHelper from "../Common/AuthHelper";
 const API_URL = "https://api.adoletarts.com.br/v1/api/";
 // const API_URL = `${process.env.REACT_APP_API_URL}`;
 
 
 let axiosConfig = {
     headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json"
     }
-  };
+};
 
 class AuthService {
     login(email: string, password: string) {
@@ -15,9 +16,10 @@ class AuthService {
             .post(API_URL + "auth", {
                 email,
                 password
-            },axiosConfig)
+            }, axiosConfig)
             .then(response => {
                 if (response.data.data.token) {
+                    AuthHelper.ClearSession()
                     localStorage.setItem("accessToken", JSON.stringify(response.data));
                 }
                 return response.data;
